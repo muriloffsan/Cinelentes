bdbb0078747becc417d52817c88a92dc9441a6dc
muriloffsan <muriloff.santana@gmail.com>
2025-03-20 10:16:08 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar CineLentes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="navbar-container">
+        <nav class="navbar">
+            <div class="navbar-left">
+                <img src="./imagens/Sesi_Senai.png" alt="SESI SENAI">
+            </div>
+            <div class="navbar-center">
+                <img src="/imagens/logo-cinelentes.png" alt="CineLentes" class="logo">
+            </div>
+        </nav>
+        <div class="navbar-divider"></div>
+        <div class="navbar-menu-container">
+            <ul class="navbar-menu">
+                <li><a href="#">INÍCIO</a></li>
+                <li><a href="#">EDIÇÕES</a></li>
+                <li><a href="#">QUEM SOMOS</a></li>
+                <li><a href="#">AGENDA</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main>
+        <section class="introducao">
+            <h1>Introdução</h1>
+            <p>O cinelentes é um projeto</p>
+        </section>
+    </main>
+</body>
+</html>
+
