c7304e8c2468f660ea7424dbaf195e3a2ad56f29
Miguel Sales <miguelfranciscodasilvasalessal@gmail.com>
2025-04-28 20:15:16 -0300

    commit acervos adm-user-pagina edição e exclusão user

---
+++ php/edicao2025.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial - Cinelentes</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="./js/main.js"></script>
+</head>
+<body class="body-pagina-inicial">
+    
+    <main class="main-acervos">
+    <section class="acervo">
+      <div class="titulo-acervo">
+        <h1 class="titulo-acervo-h1">Acervo Cinelentes - 2025</h1>
+        <div class="linha-preta-acervo-titulo"></div>
+
+      </div>
+      
+
+      <div class="cards">
+        <div class="card">
+          <img src="caminho-da-sua-imagem-1.png" alt="Festival Mês da Mulher">
+          <div class="card-text">1° Festival Cinelentes<br>Mês da Mulher</div>
+        </div>
+
+        <div class="card">
+          <img src="caminho-da-sua-imagem-2.png" alt="Festival LGBTQIAPN+">
+          <div class="card-text">4° Festival Cinelentes<br>LGBTQIAPN+</div>
+        </div>
+
+        <div class="card">
+          <img src="caminho-da-sua-imagem-3.png" alt="Festival Povos Originários">
+          <div class="card-text">2° Festival Cinelentes<br>Povos Originários</div>
+        </div>
+
+        <div class="card">
+          <img src="caminho-da-sua-imagem-4.png" alt="Festival Inclusão">
+          <div class="card-text">5° Festival Cinelentes<br>Inclusão</div>
+        </div>
+
+        <div class="card">
+          <img src="caminho-da-sua-imagem-5.png" alt="Festival Mês do Trabalho">
+          <div class="card-text">3° Festival Cinelentes<br>Mês do Trabalho</div>
+        </div>
+
+        <div class="card">
+          <img src="caminho-da-sua-imagem-6.png" alt="Festival Consciência Negra">
+          <div class="card-text">6° Festival Cinelentes<br>Povos Consciência Negra</div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer class="footer-container-acervo">
+    <div class="footer-topo-acervo">
+        <div class="footer-logo-container-acervo">
+            <img id="logo-cinelentes-footer-acervo" src="../img/logo-cinelentes.png" alt="CineLentes">
+        </div>
+
+        <div class="botao-login-container-acervo">
+            <a href="./php/login.php" class="botao-login-acervo">Login Administrador</a>
+        </div>
+    </div>
+
+    <div class="linha-branca-footer-acervo"></div>
+
+    <div class="linha-preta-footer-acervo">
+        <p class="footer-direitos-acervo">Todos os direitos reservados.</p>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ php/pagina-edicao-exclusao-criar-eventos-adm.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Acervo Cinelentes</title>
+  <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+  <main class="main-acervos">
+
+    <div class="topo-acervo">
+        <button class="botao-criar-projeto">+ Criar Projeto</button>
+    </div>
+    
+
+    <!-- Acervo 2025 -->
+    <section class="acervo">
+      <div class="titulo-acervo">
+        <h1 class="titulo-acervo-h1">Acervo Cinelentes - 2025</h1>
+        <div class="linha-preta-acervo-titulo"></div>
+
+      </div>
+
+      <div class="cards">
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="1° Festival 2023">
+          <div class="card-text">
+            1° Festival Cinelentes<br>Mês da Mulher
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="2° Festival 2023">
+          <div class="card-text">
+            2° Festival Cinelentes<br>LGBTQIA+
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="3° Festival 2023">
+          <div class="card-text">
+            3° Festival Cinelentes<br>Povos Originários
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="4° Festival 2023">
+          <div class="card-text">
+            4° Festival Cinelentes<br>Inclusão
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="5° Festival 2023">
+          <div class="card-text">
+            5° Festival Cinelentes<br>Mês do Trabalho
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="6° Festival 2023">
+          <div class="card-text">
+            6° Festival Cinelentes<br>Povos Conhecimento Negro
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Acervo 2024 -->
+    <section class="acervo">
+      <div class="titulo-acervo">
+        <h1 class="titulo-acervo-h1">Acervo Cinelentes - 2024</h1>
+        <div class="linha-preta-acervo-titulo"></div>
+
+      </div>
+
+      <div class="cards">
+        <div class="card">
+          <img id="img-card" src="" alt="1° Festival 2024">
+          <div class="card-text">
+            1° Festival Cinelentes<br>Mês da Mulher
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="2° Festival 2024">
+          <div class="card-text">
+            2° Festival Cinelentes<br>LGBTQIA+
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="3° Festival 2024">
+          <div class="card-text">
+            3° Festival Cinelentes<br>Povos Originários
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="4° Festival 2024">
+          <div class="card-text">
+            4° Festival Cinelentes<br>Inclusão
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="5° Festival 2024">
+          <div class="card-text">
+            5° Festival Cinelentes<br>Mês do Trabalho
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="6° Festival 2024">
+          <div class="card-text">
+            6° Festival Cinelentes<br>Povos Conhecimento Negro
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Acervo 2023 -->
+    <section class="acervo">
+      <div class="titulo-acervo">
+        <h1 class="titulo-acervo-h1">Acervo Cinelentes - 2023</h1>
+        <div class="linha-preta-acervo-titulo"></div>
+      </div>
+
+      <div class="cards">
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="1° Festival 2025">
+          <div class="card-text">
+            1° Festival Cinelentes<br>Mês da Mulher
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="2° Festival 2025">
+          <div class="card-text">
+            2° Festival Cinelentes<br>LGBTQIA+
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="3° Festival 2025">
+          <div class="card-text">
+            3° Festival Cinelentes<br>Povos Originários
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="4° Festival 2025">
+          <div class="card-text">
+            4° Festival Cinelentes<br>Inclusão
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="5° Festival 2025">
+          <div class="card-text">
+            5° Festival Cinelentes<br>Mês do Trabalho
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+
+        <div class="card">
+          <img id="img-card" src="bitelo.png" alt="6° Festival 2025">
+          <div class="card-text">
+            6° Festival Cinelentes<br>Povos Conhecimento Negro
+          </div>
+          <div class="card-buttons">
+            <button class="botao-editar">EDITAR</button>
+            <button class="botao-excluir">EXCLUIR</button>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Agenda -->
+    <section class="agenda">
+      <h2 class="agenda-h2">Agenda</h2>
+      <textarea class="areatexto-agenda"  placeholder="Digite a data de algum projeto que irá acontecer..." rows="4"></textarea>
+      <button class="botao-confirmar">Confirmar</button>
+    </section>
+
+  </main>
+
+  <footer class="footer-container-acervo">
+    <div class="footer-topo-acervo">
+      <img id="logo-cinelentes-footer-acervo" src="logo.png" alt="Logo Cinelentes">
+    </div>
+    <div class="linha-branca-footer-acervo"></div>
+    <div class="linha-preta-footer-acervo">
+      <p class="footer-direitos-acervo">© 2025 Todos os direitos reservados</p>
+    </div>
+  </footer>
+
+</body>
+</html>
